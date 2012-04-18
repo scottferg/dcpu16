41f24f3b250b28a096e6c1cb62973f9437b578aa
Scott Ferguson <scottwferg@gmail.com>
2012-04-17 22:36:27 -0500

    Enable color detection

--- video.go
+++ video.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/nsf/termbox-go"
+    "fmt"
 )
 
 type Video struct {}
@@ -19,17 +20,17 @@ func GetColor(color Word) (termbox.Attribute) {
     case 0x0:
         return termbox.ColorBlack
     case 0x1:
-        return termbox.ColorRed
+        return termbox.ColorBlue
     case 0x2:
         return termbox.ColorGreen
     case 0x3:
-        return termbox.ColorYellow
+        return termbox.ColorCyan
     case 0x4:
-        return termbox.ColorBlue
+        return termbox.ColorRed
     case 0x5:
         return termbox.ColorMagenta
     case 0x6:
-        return termbox.ColorCyan
+        return termbox.ColorYellow
     case 0x7:
         return termbox.ColorWhite
     }
@@ -44,14 +45,15 @@ func (v *Video) DrawScreen() {
     for y := 0; y < 16; y++ {
         for x := 0; x < 32; x++ {
             data := Memory[0x8000 + i]
+            fmt.Printf("0x%x\n", data)
 
+            character := rune(data & 0x7f)
             colors := data >> 7
 
             fg := GetColor(colors & 0xf)
             bg := GetColor(colors >> 4)
 
-            character := rune(data & 0x7f)
-            termbox.SetCell(x, y, character, fg, bg)
+            termbox.SetCell(x, y, character, bg, fg)
 
             i++
         }
